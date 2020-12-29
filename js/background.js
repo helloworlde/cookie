79af0962b973176045b4d79747444b55dc316cc3
helloworlde <hehuimin03@meituan.com>
2020-12-29 17:45:44 +0800

    Read setting when send cookie

--- js/background.js
+++ js/background.js
@@ -1,51 +1,66 @@
-function loadSettings(name) {
-    return "http://localhost:8080/cookies";
-}
-
-
 function updateCookie(domain, cookie) {
-    let serverUrl = loadSettings("serverUrl")
-    let token = loadSettings("token")
-    $.ajax({
-        url: serverUrl,
-        type: "POST",
-        contentType: "application/json;charset=utf-8",
-        headers: {
-            "Authentication": token,
-        },
-        data: JSON.stringify({
-            "cookie": cookie,
-            "domain": domain
-        }),
-        success: (data) => {
-            console.log("Update cookie success: " + data)
-        },
-        error: (xhr, status, err) => {
-            console.error("Update cookie failed,  status: " + status + " err: " + err);
+    chrome.storage.sync.get(["serverAddress"], function (result) {
+        console.log("serverAddress: " + JSON.stringify(result));
+
+        if (result.serverAddress === undefined) {
+            return;
         }
-    });
+
+        let serverUrl = result.serverAddress;
+        chrome.storage.sync.get(["token"], function (result) {
+            console.log("token: " + JSON.stringify(result));
+            if (result.token === undefined) {
+                result.token = "";
+            }
+            let token = result.token;
+
+            $.ajax({
+                url: serverUrl,
+                type: "POST",
+                contentType: "application/json;charset=utf-8",
+                headers: {
+                    "Authentication": token,
+                },
+                data: JSON.stringify({
+                    "cookie": cookie,
+                    "domain": domain
+                }),
+                success: (data) => {
+                    console.log("Update cookie success: " + data)
+                },
+                error: (xhr, status, err) => {
+                    console.error("Update cookie failed,  status: " + status + " err: " + err);
+                }
+            });
+        })
+    })
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    chrome.cookies.getAll({"domain": request.domain}, function (cookies) {
-        let cookie = cookies.map((item) => {
-            return parseNeedCookie(item)
-        }).filter(value => value !== "").join(";");
+    chrome.storage.sync.get(["cookieNames"], function (result) {
+        if (result.cookieNames === undefined) {
+            result.cookieNames = "";
+        }
+        let cookieNames = result.cookieNames.split(",");
+        chrome.cookies.getAll({"domain": request.domain}, function (cookies) {
+            let cookie = cookies.map((item) => {
+                return parseNeedCookie(cookieNames, item)
+            }).filter(value => value !== "").join(";");
 
-        updateCookie(request.domain, cookie)
-    });
+            updateCookie(request.domain, cookie)
+        });
+    })
+
     sendResponse("Save cookie completed");
 });
 
-let keyWhiteList = [
-    "ssoid",
-    "com.sankuai.it.ead.citadel_ssoid",
-]
 
-function parseNeedCookie(cookie) {
-    if (keyWhiteList.indexOf(cookie.name) !== -1) {
-        return cookie.name + "=" + cookie.value
-    } else {
-        return "";
+function parseNeedCookie(cookieNames, cookie) {
+    console.log(cookieNames)
+    if (cookieNames.length > 0) {
+        if (cookieNames.indexOf(cookie.name) !== -1) {
+            return cookie.name + "=" + cookie.value;
+        }
     }
+    return "";
 }
